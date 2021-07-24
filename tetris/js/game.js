d02bcda8f9bc8e260bbf6c3fccd35724ee4b9e5a
Yurii <76671367+jury16@users.noreply.github.com>
2021-07-24 08:02:46 -0400

    Uppload file

--- tetris/js/game.js
+++ tetris/js/game.js
@@ -1,11 +1,19 @@
 "use strict"
 
 const canvas = document.getElementById('game'),
-    canvasInfo = document.getElementById('info'),
+    menu = document.getElementById('menuBody'),
+    iconMenu = document.getElementById('menuIcon'),
+    commun = document.getElementById('canvasOptionsBlock'),
     gameCanvas = document.getElementById('canvas'),
-    optionsCanvas = document.getElementById('optionsInfoBlock'),
+    statistique = document.getElementById('statistique'),
+    rotateDisplay = document.getElementById('rotateDisplay'),
+    gameInfoS = document.getElementById('gameInfo'),
     buttons = document.getElementById('buttons'),
+    menuOptions = document.getElementById('optionsBlock'),
+    newGameBlock = document.getElementById('newGameBlock'),
     newGame = document.getElementById('newGame'),
+    textOptions = document.createElement('p'),
+    infoGame = document.createElement('p'),
     pauseGame = document.getElementById('pause'),
     wrapper = document.getElementById('wrapper'),
     visible = document.getElementById('visible'),
@@ -13,14 +21,9 @@ const canvas = document.getElementById('game'),
     buttonRight = document.getElementById('right'),
     buttonDown = document.getElementById('down'),
     buttonRotate = document.getElementById('rotate'),
-    infoCanvas = document.getElementById('infoCanvas'),        
-    contextLeft = buttonLeft.getContext('2d'),
-    contextRight = buttonRight.getContext('2d'),
-    contextDown = buttonDown.getContext('2d'),
-    contextRotate = buttonRotate.getContext('2d'),
-    context = canvas.getContext('2d'),
-    contextOptions = optionsCanvas.getContext('2d'),
-    contextInfo = canvasInfo.getContext('2d');
+    infoCanvas = document.getElementById('infoCanvas'),
+    context = canvas.getContext('2d');
+
 var playAnimation = null,
     gameOver = false,
     mobileDevice = false,
@@ -30,6 +33,9 @@ var playAnimation = null,
     record = 0,
     pauseFlag = false,
     optionsFlag = false,
+    newGamesFlag = false,
+    rotateFlag = false,
+    zoomGame = 1.2,
     level = 1,
     bestPlayer = '',
     namePlayer = "Vasia",
@@ -49,10 +55,14 @@ var playAnimation = null,
     infoRecordY;
 checkScreenWidth = document.body.clientWidth;
 checkScreenHeight = document.body.clientHeight;
+menuOptions.appendChild(textOptions);
+console.log(canvas.getBoundingClientRect().left);
+
+statistique.appendChild(infoGame);
 // make gabarits canvas depending on the screen
 gabarits(visible.offsetWidth * 0.8);
 function gabarits(width) {
-        canvasWidth = width ,
+        canvasWidth = width,
         canvasHeight = canvasWidth * 2,
         infoWidth = canvasWidth * 0.9,
         infoHeight = canvasWidth * 0.3,
@@ -64,27 +74,32 @@ function gabarits(width) {
         infoRecordY = 0.12 * canvasHeight;
     canvas.width = canvasWidth;
     canvas.height = canvasHeight;
-    optionsCanvas.width = canvasWidth;
-    optionsCanvas.height = canvasHeight;
-    canvasInfo.width = infoWidth;
-    canvasInfo.height = infoHeight;
-    buttonLeft.width = canvasWidth / 2;
-    buttonLeft.height = canvasHeight / 8;
-    buttonRight.width = canvasWidth / 2;
-    buttonRight.height = canvasHeight / 8;
-    buttonDown.width = canvasWidth / 2;
-    buttonDown.height = canvasHeight / 8;
-    buttonRotate.width = canvasWidth / 2;
-    buttonRotate.height = canvasHeight / 8;
+    menuOptions.style.opacity = '0';
+    newGameBlock.style.opacity = '0';
+    buttons.style.width = width + 'px';
+
 }
-//console.log(canvasWidth);
-//console.log(canvasHeight);
+//blocks for animated messages
+var positionMessagesLeft = `-${canvasWidth * 1.35}px`,
+    positionMessagesTop = `${canvasHeight * 0.0485}px`;
+function createBlocksFunc(width, height, left = positionMessagesLeft, top = positionMessagesTop){
+    creatBlocks(menuOptions);
+    creatBlocks(newGameBlock);
+    function creatBlocks(name) {
+        name.style.width = `${width * 0.98}px`;
+        name.style.height = `${height}px`;
+        name.style.position = 'absolute';
+        name.style.left = left;
+        name.style.top = top;
+        name.style.backgroundColor = 'black';
+    }
+}
+createBlocksFunc(canvasWidth, canvasHeight);
 
 //create a media condition that checks the viewports to be at least 768 pixels wide.
 const mediaQuery1 = window.matchMedia('(max-width: 749px)');
 const mediaQuery2 = window.matchMedia('(min-width: 751px)');
 var flag = false;
-var flagGabarits = false;
 //check if mobile device and landscape screen
 function deviceType(){
 
@@ -222,6 +237,7 @@ function placeFigure() {
     for (let row = playZone.length - 1; row >= 0;) {
         if (playZone[row].every(cell => !!cell)) {
             score += 10;
+            gameInfo(score);
             level = Math.floor(score / 100) + 1;
             for (let r = row; r >= 0; r--) {
                 for (let c = 0; c < playZone[r].length; c++) {                    
@@ -242,7 +258,7 @@ function showGameOver() {
     // message Game Over
     context.fillStyle = 'black';
     context.globalAlpha = 0.75;
-    context.fillRect(0, canvas.height / 2 - 30, canvas.width, 60);
+    context.fillRect(0, canvas.height / 2 - 30, canvas.width - 10, 60);
     context.globalAlpha = 1;
     context.fillStyle = 'white';
     context.font = '36px monospace';
@@ -280,6 +296,15 @@ window.addEventListener("keydown", function (EO) {
         figureCurrent.row = row;
     }
 });
+//console.log(iconMenu);
+if (iconMenu) {
+    //const menu = document.getElementById('menu');
+    iconMenu.addEventListener('click', function (e) {
+        iconMenu.classList.toggle('_active');
+        menu.classList.toggle('_active');
+    });
+}
+
 
 //listen buttons
 
@@ -313,128 +338,201 @@ function downTouch() {
         placeFigure();
         return;
     }
-    figureCurrent.row = row;    
-}
-// game menu
-
-//game info
-function textFonts(){
-    if (document.body.clientWidth > 900) {
-        textFont = 36;
+    figureCurrent.row = row;
+    // check record
+    if (score > record) {
+        record = score;
+        localStorage.record = record;
+        bestPlayer = namePlayer;
+        localStorage.recordName = recordName;
     }
-    else if (document.body.clientWidth < 400) {
-        textFont = 22;
+    //storage
+    var Storage_size = localStorage.length;
+    if (Storage_size > 0) {
+        record = localStorage.record;
+        bestPlayer = localStorage.bestPlayer;
     }
-    else {
-        textFont = 28;
-    }
-}
-function info() {
-    textFonts();
-    contextInfo.clearRect(0, 0, canvasInfo.width, canvasInfo.height);
-    contextInfo.globalAlpha = 1;
-    contextInfo.fillStyle = 'white';
-    contextInfo.font = `${textFont}px Arial New`;
-    contextInfo.fillText('Level:' + level, infoLevelX, infoLevelY);
-    contextInfo.fillText('Points:' + score, infoRecordX, infoLevelY);
-    //contextInfo.fillText('Best player: ' + bestPlayer, infoRecordX, infoRecordY);
-    contextInfo.fillText('Record:  ' + record, infoLevelX, infoRecordY);
-
-    //info buttons
-    function infoButtons(name, textButton, x, y) {
-        name.clearRect(0, 0, buttonLeft.width, buttonLeft.height);
-        name.globalAlpha = 1;
-        name.fillStyle = 'red';
-        name.font = `${textFont}px Arial New`;
-        name.fillText(`${textButton}`, x * canvasWidth * 0.12, y * canvasHeight * 0.075);
-    }
-    infoButtons(contextLeft, "LEFT", 1.1, 1);
-    infoButtons(contextRight, "RIGHT", 1, 1);
-    infoButtons(contextDown, "DOWN", 0.9, 1.1);
-    infoButtons(contextRotate, "ROTATE", 0.75, 1.1);
-}
-// check record
-if (score > record) {
-    record = score;
-    localStorage.record = record;
-    bestPlayer = namePlayer;
-    localStorage.recordName = recordName;
-}
-//storage
-var Storage_size = localStorage.length;
-if (Storage_size > 0) {
-    record = localStorage.record;
-    bestPlayer = localStorage.bestPlayer;
-}
-//start New Game
-function gameStart() {
-    level = 1;
-    score = 0;    
-    for (let row = -2; row < 20; row++) {
-        playZone[row] = [];
-        for (let col = 0; col < 10; col++) {
-            playZone[row][col] = 0;
+    //start New Game
+    function gameStart() {
+       // debugger;
+        if (pauseFlag) { pause1(newGamesFlag) };
+        level = 1;
+        score = 0;
+        for (let row = -2; row < 20; row++) {
+            playZone[row] = [];
+            for (let col = 0; col < 10; col++) {
+                playZone[row][col] = 0;
+            }
+        }
+        figureCurrent = nextFigure();
+        if (gameOver) {
+            game();
+            gameOver = false;
         }
     }
-    figureCurrent = nextFigure();
-    if (gameOver) {
-        game();
-        gameOver = false;
-        console.log('yes');
-    }
-    
-  
 }
-function options() {    
-    if (optionsFlag == false) {
-        optionsFlag = true;
+//size of text
+function textFonts(){
+    if (document.body.clientWidth > 900) {
+        textFont = 16;
+    }
+    else if (document.body.clientWidth < 600) {
+        textFont = 12;
+    }
+    else {
+        textFont = 14;
+    }
+    return textFont;
+}
+
+// text options block
+function createMessages(name, message, text = textFonts(), align = 'center') {
+    name.style.fontSize = `${text}px`;
+    name.style.textTransform = 'uppercase';
+    name.style.color = 'white';
+    name.style.textAlign = align;
+    name.innerHTML = message;
+    //name.style.width = canvasWidth * 1.5;
+    //name.style.height = canvasHeight * 0.75;
+}
+var messageOptionsBlock;
+if (!messageOptionsBlock) {
+    messageOptionsBlock = '<div class="optionsMessage"><p>LEFT&emsp;&emsp;&larr;</p><p>RIGHT&emsp;&thinsp;&thinsp;&rarr;</p><p>DOWN&emsp;&thinsp;&thinsp;&thinsp;&thinsp;&darr;</p><p>ROTATE&emsp;&thinsp;&uarr;</p></div>';
+    var messageNewGame = `<div class="messageNewGame"><p>start new game?</p><p><button id="yes" href="#" onclick="newGames()">YES</button><p><button id="no"href="#" onclick="gameStart()">NO</button></div>`;
+    var messageGameInfo = `<div class="infoGame"><p>level:&emsp;&thinsp; ${level}&emsp;points:&emsp; ${score}</p><p>name:&emsp;${namePlayer}&emsp;</p><p>record:&emsp;${record} </a></div>`;
+    var messageRotateInfo = '<div class=""><p>rotate display</p></div>';
+    createMessages(textOptions, messageOptionsBlock);
+    createMessages(newGameBlock, messageNewGame);
+    createMessages(infoGame, messageGameInfo, 14, 'left');
+    createMessages(rotateDisplay, messageRotateInfo, 14, 'left');
+    console.log('yes');
+}
+
+/*
+var yes = document.getElementById('yes');
+console.log(yes);
+var no = document.getElementById('no');
+*/
+
+//createMessages(wrapper, messageRotateInfo);
+//console.log(infoGame);
+/*
+yes.addEventListener('click', function (e) {
+    newGames();
+});
+no.addEventListener('click', function (e) {
+    gameStart();
+});
+*/
+
+//show animation messages
+function transitionON(name) {
+    name.style.opacity = '1';
+    name.style.transform = `translate(${canvasWidth * 1}px, 0px)`;
+    name.style.transition = 'all 1.0s ease';
+    //debugger;
+}
+function transitionOff(name) {
+    name.style.opacity = '0';
+    name.style.transform = `translate(${0}px, 0px)`;
+    name.style.transition = 'all 1.0s ease';
+}
+function transitions(name,transitionsFlag) {
+    if (!transitionsFlag) {
+        transitionsFlag = true;
         if (!pauseFlag) {
             pause1();
-        }  
-        
+            pauseFlag = true;
+            transitionON(name);
+        }
+        else if (pauseFlag) {
+            pauseFlag = false;
+            transitionsFlag = false;
+            pauseGame.innerText = 'pause';
+            transitionOff(name);
+        }       
     }
-
-    else if (pauseFlag == true) {
+    else if (transitionsFlag) {
         pauseFlag = false;
-        pauseGame.innerText = 'pause';
-
+        transitionsFlag = false;
+        transitionOff(name);
+        pauseGame.innerText = 'continue';
     }
 }
-function pause1() {
-    if (pauseFlag == false) {
+function newGames() {
+    checkPause(menuOptions);
+    transitions(newGameBlock, newGamesFlag);
+}
+
+function checkPause(name) {
+    if (pauseFlag) {
+        pauseFlag = false;
+        transitionOff(name);
+    }
+}
+function options() {
+    checkPause(newGameBlock);
+    transitions(menuOptions, optionsFlag);
+}
+function rotateDispleY() {
+    if (rotateFlag) {
+        rotateDisplay.style.display = 'flex';
+        visible.style.display = 'none';
+    }
+    
+}
+function pause1(transitionsFlag) {
+    if (!pauseFlag) {
         pauseFlag = true;
         pauseGame.innerText = 'continue';
     }
-    else if(pauseFlag == true) {
+    else if (pauseFlag) {
         pauseFlag = false;
-        optionsFlag = false;
+        transitionsFlag = false;
         pauseGame.innerText = 'pause';
-
+        transitionOff(menuOptions);
+        transitionOff(newGameBlock);
     }
+
 }
+//check device
+function device() {
+    if ((mediaQuery1.matches)) {
+        if (mobileDevice) {
+
+        }
+        visible.style.flexDirection = 'column';
+        gameInfoS.style.order = '2';
+        commun.style.order = '3';
+        visible.style.width = 34 + 'vh';
+        var positionMessagesLeft = `-${canvasWidth * 0.96}px`,
+            positionMessagesTop = `${canvasHeight * 0.049 - statistique.offsetHeight * 0.33}px`;
+        createBlocksFunc(canvasWidth, canvasHeight, positionMessagesLeft, positionMessagesTop);
+        buttons.style.display = 'flex';
+    }
+    else if ((mediaQuery2.matches)) {
+        buttons.style.display = 'none';
+        visible.style.flexDirection = 'row';
+        gameInfoS.style.order = '3';
+        commun.style.order = '2';
+        visible.style.width = 40 + 'vh';        
+        createBlocksFunc(canvasWidth, canvasHeight);
+    }
+    createMessages(textOptions, messageOptionsBlock);
+    createMessages(newGameBlock, messageNewGame);
+    createMessages(infoGame, messageGameInfo, 14, 'left');
+    //gabarits(visible.offsetWidth * 0.8);
+}
+device();
 //game
 function game() {
-
-    if (!mobileDevice) {
-        buttons.style.display = 'none'
-    }
-
-    if ((mediaQuery1.matches  && !flag)) {
-        visible.style.flexDirection = 'column';
-        gameCanvas.style.order = '2';
-        infoCanvas.style.order = '1';
-        flag = true;
-    }
-    else if ((mediaQuery2.matches && flag)) {
-        visible.style.flexDirection = 'row';
-        gameCanvas.style.order = '1';
-        infoCanvas.style.order = '2';
-        flag = false;
-    }
+    device();//check this!
+    deviceType();
+    //console.log(canvasWidth);
+   // debugger;
 
     playAnimation = requestAnimationFrame(game);
     context.clearRect(0, 0, canvas.width, canvas.height);
-    info();
     for (let row = 0; row < 20; row++) {
         for (let col = 0; col < 10; col++) {
             if (playZone[row][col]) {
@@ -445,12 +543,22 @@ function game() {
         }
     }
     // check if screen changed
+    if ((mobileDevice && (document.body.clientWidth > document.body.clientHeight))) {
+        rotateFlag = true;
+        pauseFlag = true;
+        rotateDispleY();
+        //console.log('yes');        
+    }
+    else {
+        rotateFlag = false;
+        rotateDisplay.style.display = 'none';
+        visible.style.display = 'flex';
+        
+    }
     if (figureCurrent) {      
-        if ((mobileDevice && (document.body.clientWidth > document.body.clientHeight && document.body.clientWidth < 800)) || pauseFlag == true) {
-            figureCurrent.row;
-            //pauseFlag = false;
-        }
-        else if (++count > 60) {
+        
+        if (++count > 60) {
+            
             figureCurrent.row++;
             count = 0;
             if (!isValidMove(figureCurrent.matrix, figureCurrent.row, figureCurrent.col)) {
@@ -458,7 +566,6 @@ function game() {
                 placeFigure();
             }
         }
-        
         context.fillStyle = colors[figureCurrent.name];
         for (let row = 0; row < figureCurrent.matrix.length; row++) {
             for (let col = 0; col < figureCurrent.matrix[row].length; col++) {
